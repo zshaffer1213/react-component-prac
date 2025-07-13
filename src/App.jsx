0232520a27a699e9b0803756b0095de905ab8a97
zshaffer1213 <zshaff1213@gmail.com>
2025-07-13 11:55:34 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,21 @@ export default function App() {
 
   const [searchQ, setSearchQ] = useState('')
 
-  
+  const normalizeSearch = searchQ.toLowerCase()
+
+  const filteredRecipes = recipes.filter((recipe) => {
+        const titleMatch = recipe.title.toLowerCase().includes(normalizeSearch)
+        const itemMatch = recipe.ingredients.some(ingredient => ingredient.toLowerCase().includes(normalizeSearch))
+        return titleMatch || itemMatch
+  })
 
   return (
     <>
       <input type="text" value={searchQ} onChange={(e) => setSearchQ(e.target.value)} placeholder="Search Recipes..." />
-      {recipes.filter((recipe) => {
-        const titleMatch = recipe.title.toLowerCase().includes(searchQ.toLowerCase())
-        const itemMatch = recipe.ingredients.some(ingredient => ingredient.toLowerCase().includes(searchQ.toLowerCase()))
-        return titleMatch || itemMatch
-      }).map((recipe, index) => (
+      {filteredRecipes.length >= 1 && filteredRecipes.map((recipe, index) => (
         <RecipeCard key={index} {...recipe} />
       ))}
+      {filteredRecipes.length === 0 && <p>No Matches for your search criteria.</p>}
     </>
   )
 }
