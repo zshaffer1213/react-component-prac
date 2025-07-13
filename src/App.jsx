b3c42a9b09eb10f3e073ad238309f215a5de9748
zshaffer1213 <zshaff1213@gmail.com>
2025-07-12 23:23:13 -0400

    adding filter by search

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import recipes from "./recipes";
 import RecipeCard from "./RecipeCard";
 
 export default function App() {
+
+  const [searchQ, setSearchQ] = useState('')
+
+  
+
   return (
     <>
-      {recipes.map((recipe, index) => (
+      <input type="text" value={searchQ} onChange={(e) => setSearchQ(e.target.value)} placeholder="Search Recipes..." />
+      {recipes.filter((recipe) => {
+        const titleMatch = recipe.title.toLowerCase().includes(searchQ.toLowerCase())
+        const itemMatch = recipe.ingredients.some(ingredient => ingredient.toLowerCase().includes(searchQ.toLowerCase()))
+        return titleMatch || itemMatch
+      }).map((recipe, index) => (
         <RecipeCard key={index} {...recipe} />
       ))}
     </>
--- src/IntructionsToggle.jsx
+++ src/IntructionsToggle.jsx
@@ -2,12 +2,23 @@ import { useState } from "react";
 
 export default function InstructionsToggle(props) {
 
-    const [showInstructions, setShowInstructions] = useState(props.defaultOpen)    
+    const [showInstructions, setShowInstructions] = useState(props.defaultOpen) 
+    
+    const steps = props.instructions
+        .split(',')
+        .map(step => step.trim())
+        .filter(step => step.length > 0)
 
     return (
         <>
             <button onClick={() => setShowInstructions(prev => !prev)}>{showInstructions ? "Hide Instructions" : "Show Instructions"}</button>
-            {showInstructions && <p>{props.instructions}</p>}
+            {showInstructions && (
+                <ul>
+                    {steps.map((step, index) => (
+                        <li key={index}>{step}</li>
+                    ))}
+                </ul>
+            )}
         </>
     )
 
